c4a08e9302c87f0b9fa9643024e93c0dc9dee73e
tst22398 <115932966+tst22398@users.noreply.github.com>
2023-07-08 22:47:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 const httpServer = createServer(app);
